3f18ad38ddeb0a33671dd57393bc0ac3f7129381
airfight <745756619@qq.com>
2017-03-06 16:34:40 +0800

    //

---
+++ GYNetWorking/DownLoad/GYDownLoadPath.swift
@@ -0,0 +1,20 @@
+//
+//  GYDownLoadPath.swift
+//  GYNetWorking
+//
+//  Created by ZGY on 2017/3/6.
+//  Copyright © 2017年 Giant. All rights reserved.
+//
+//  Author:        Airfight
+//  My GitHub:     https://github.com/airfight
+//  My Blog:       http://airfight.github.io/
+//  My Jane book:  http://www.jianshu.com/users/17d6a01e3361
+//  Current Time:  2017/3/6  16:03
+//  GiantForJade:  Efforts to do my best
+//  Real developers ship.
+
+import UIKit
+
+class GYDownLoadPath: NSObject {
+
+}
