3118eb5cfa8642d0b0ae75f98d80ba019932b853
airfight <745756619@qq.com>
2017-03-07 22:02:04 +0800

    不懂

--- GYNetWorking/DownLoad/DownLoadManager.swift
+++ GYNetWorking/DownLoad/DownLoadManager.swift
@@ -15,3 +15,96 @@
 
 import UIKit
 
+/// A dictionary of headers to apply to a `URLRequest`.
+public typealias HTTPHeaders = [String: String]
+
+open class DownloadManger {
+    
+    open static let `default`: DownloadManger = {
+        
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = DownloadManger.defaultHTTPHeaders
+        
+        return DownloadManger(configuration: configuration)
+    }()
+    
+    open static let defaultHTTPHeaders: HTTPHeaders = {
+        
+        let acceptEncoding: String = "gzip;q=1.0, compress;q=0.5"
+        
+        // Accept-Language HTTP Header; see https://tools.ietf.org/html/rfc7231#section-5.3.5
+        let acceptLanguage = Locale.preferredLanguages.prefix(6).enumerated().map { index, languageCode in
+            let quality = 1.0 - (Double(index) * 0.1)
+            return "\(languageCode);q=\(quality)"
+            }.joined(separator: ", ")
+    
+        let userAgent: String = {
+            if let info = Bundle.main.infoDictionary {
+                let executable = info[kCFBundleExecutableKey as String] as? String ?? "Unknown"
+                let bundle = info[kCFBundleIdentifierKey as String] as? String ?? "Unknown"
+                let appVersion = info["CFBundleShortVersionString"] as? String ?? "Unknown"
+                let appBuild = info[kCFBundleVersionKey as String] as? String ?? "Unknown"
+                
+                let osNameVersion: String = {
+                    let version = ProcessInfo.processInfo.operatingSystemVersion
+                    let versionString = "\(version.majorVersion).\(version.minorVersion).\(version.patchVersion)"
+                    
+                    let osName: String = {
+                        #if os(iOS)
+                            return "iOS"
+                        #elseif os(watchOS)
+                            return "watchOS"
+                        #elseif os(tvOS)
+                            return "tvOS"
+                        #elseif os(macOS)
+                            return "OS X"
+                        #elseif os(Linux)
+                            return "Linux"
+                        #else
+                            return "Unknown"
+                        #endif
+                    }()
+                    
+                    return "\(osName) \(versionString)"
+                }()
+                
+                let alamofireVersion: String = {
+                    guard
+                        let afInfo = Bundle(for: DownloadManger.self).infoDictionary,
+                        let build = afInfo["CFBundleShortVersionString"]
+                        else { return "Unknown" }
+                    
+                    return "Alamofire/\(build)"
+                }()
+                
+                return "\(executable)/\(appVersion) (\(bundle); build:\(appBuild); \(osNameVersion)) \(alamofireVersion)"
+            }
+            
+            return "Alamofire"
+        }()
+        
+        return [
+            "Accept-Encoding": acceptEncoding,
+            "Accept-Language": acceptLanguage,
+            "User-Agent": userAgent
+        ]
+    }()
+
+        
+        
+    
+    
+    open let delegate: GYSessionDelegate
+    open let session: URLSession
+    
+    public init(configuration: URLSessionConfiguration = URLSessionConfiguration.default,
+                delegate: GYSessionDelegate = GYSessionDelegate()) {
+        
+        
+        self.delegate = delegate
+        self.session = URLSession(configuration: configuration, delegate: delegate, delegateQueue: nil)
+        
+        
+    }
+    
+}
