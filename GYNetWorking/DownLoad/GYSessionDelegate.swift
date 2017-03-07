c8393b32bd312a8445375c72ffbce4fccf25ab8e
airfight <745756619@qq.com>
2017-03-07 17:55:49 +0800

    //

---
+++ GYNetWorking/DownLoad/GYSessionDelegate.swift
@@ -0,0 +1,86 @@
+//
+//  GYSessionDelegate.swift
+//  GYNetWorking
+//
+//  Created by ZGY on 2017/3/7.
+//  Copyright © 2017年 Giant. All rights reserved.
+//
+//  Author:        Airfight
+//  My GitHub:     https://github.com/airfight
+//  My Blog:       http://airfight.github.io/
+//  My Jane book:  http://www.jianshu.com/users/17d6a01e3361
+//  Current Time:  2017/3/7  17:39
+//  GiantForJade:  Efforts to do my best
+//  Real developers ship.
+
+import UIKit
+
+open class GYSessionDelegate: NSObject {
+    
+    
+    open var downloadTaskDidFinishDownloadingToURL: ((URLSession, URLSessionDownloadTask, URL) -> Void)?
+    
+    open var downloadTaskDidWriteData: ((URLSession, URLSessionDownloadTask, Int64, Int64, Int64) -> Void)?
+    
+    open var downloadTaskDidResumeAtOffset: ((URLSession, URLSessionDownloadTask, Int64, Int64) -> Void)?
+}
+
+
+extension GYSessionDelegate: URLSessionDataDelegate {
+    
+    open func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didBecome downloadTask: URLSessionDownloadTask) {
+        
+       print("didBecome")
+    }
+    
+    
+    /// 开始接受数据
+    ///
+    /// - Parameters:
+    ///   - session: session description
+    ///   - dataTask: dataTask description
+    ///   - data: data description
+    open func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        
+         print("didReceive")
+         print("开始接受数据了")
+    }
+    
+    
+    /// 下载完成或者出错
+    ///
+    /// - Parameters:
+    ///   - session: session description
+    ///   - task: task description
+    ///   - error: error description
+    open func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        
+        if (error != nil) {
+            print("出错了")
+        }
+        print("didCompleteWithError")
+        
+        guard error != nil else {
+            
+            print("出错了")
+            return
+        }
+        
+        print("完成了")
+        
+    }
+    
+    open func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+        
+    }
+    
+    open func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
+        print("urlSessionDidFinishEvents")
+    }
+    
+    
+    open func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, willCacheResponse proposedResponse: CachedURLResponse, completionHandler: @escaping (CachedURLResponse?) -> Void) {
+        print("willCacheResponse")
+    }
+    
+}
